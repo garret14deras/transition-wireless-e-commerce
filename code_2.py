863950ff7213000328bd39b0aea98d52b6c48e12
garret14deras <139816612+garret14deras@users.noreply.github.com>
2024-03-15 01:43:59 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,39 @@
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+import random
+    main()
+def main():
+
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+
+    return data
+    for item in data:    data = generate_random_data()
+if __name__ == "__main__":
+def generate_random_data():
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+    for item in data:
+import random
+def main():
+    main()
+        print(f"Random Number: {item}")
+    return data    for item in data:
+import random
+if __name__ == "__main__":
+    main()
+    data = generate_random_data()
+
+
+def generate_random_data():
+    return data
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
